2a045bc567e752727c51ff596fc547c66bd9e3f9
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2023-11-14 20:52:08 +0800

    updated backend.tf and ecs.tf to point to AWS ecs

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 # Comment out the below if you are working on local
 
 terraform {
-  backend "s3" {
+  backend "s3" {                          # This is to configure a backend, but NOT a resource (a resource will be called a resource)
     bucket = "sctp-ce3-tfstate-bucket-1"
-    key    = "ecs-fargate-sean.tfstate" # Change the value of this to yourname-tf-workspace-act.tfstate for  example
+    key    = "ecs-fargate-martin.tfstate" # Change: the value of this to yourname-tf-workspace-act.tfstate for  example
     region = "us-east-1"
   }
 }
--- ecs.tf
+++ ecs.tf
@@ -3,7 +3,7 @@
 # accordingly
 
 resource "aws_ecs_task_definition" "own_task_definition" {
-  family                = "sean-taskdef-tf" # Update accordingly
+  family                = "martin-taskdef-tf" # Change: Update accordingly
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   execution_role_arn        = var.ex_role_arn
@@ -14,7 +14,7 @@ resource "aws_ecs_task_definition" "own_task_definition" {
     image_url        = "nginx:latest"
     container_name   = "NGINX"
     log_group_region = "us-east-1"
-    log_group_name   = "/ecs/sean-taskdef-tf" # Update accordingly
+    log_group_name   = "/ecs/martin-taskdef-tf" # Change: Update accordingly
     log_group_prefix = "ecs"
   })
 }
@@ -22,13 +22,13 @@ resource "aws_ecs_task_definition" "own_task_definition" {
 # Creates an ecs cluster
 
 resource "aws_ecs_cluster" "own_cluster" {
-  name = "sean-cluster-tf" # Update accordingly
+  name = "martin-cluster-tf" # Change: Update accordingly
 }
 
 # Creates an ecs service
 
 resource "aws_ecs_service" "own_service" {
-  name             = "sean-svc-tf" # Update accordingly
+  name             = "martin-svc-tf" # Change: Update accordingly
   cluster          = aws_ecs_cluster.own_cluster.arn
   task_definition  = aws_ecs_task_definition.own_task_definition.arn
   desired_count    = 1
